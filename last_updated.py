c73ce30f277e6050c34760028bbb2c062825b1ee
Deyvison Rocha <deyvison@gmail.com>
2018-01-03 20:24:37 -0300

    Added last updated each cryptocurrency

--- last_updated.py
+++
@@ -1,9 +0,0 @@
-import os, datetime
-
-def modification_date(filename):
-    t = os.path.getmtime(filename)
-    return datetime.datetime.fromtimestamp(t).strftime("%H:%M:%S")
-
-filename = './tmp/bitcoin'
-statbuf = os.stat(filename)
-print ("Last updated: %s" % modification_date(filename))
